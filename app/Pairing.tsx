88d8a03a7af59ae477e3050b6f9e749f59a8bc15
Jasmine Sodhi <jasmine.sodhi@10xbeta.com>
2025-05-28 11:45:32 -0400

    Add checkout with submodule in all jobs

--- app/Pairing.tsx
+++ app/Pairing.tsx
@@ -14,8 +14,8 @@ import { VButton } from "@/components/common/VButton";
 import { VDeviceItem } from "@/components/common/VDeviceItem";
 import { QRCodeScanner } from "@/components/common/VQRCodeScanner";
 import { VText } from "@/components/common/VText";
-import { bondDevice } from "@/modules/tenx-mdk-ble-rn-library/src/index";
 import useDeviceStore from "@/store/useDeviceStore";
+import { bondDevice } from "../modules/tenx-mdk-ble-rn-library/src/index";
 
 export default function PairingScreen() {
   const router = useRouter();
