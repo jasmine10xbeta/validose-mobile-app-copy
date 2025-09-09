9282501775880152012c11fe17349006fde22b31
Jasmine Sodhi <jasmine.sodhi@10xbeta.com>
2025-09-09 13:01:12 -0400

    Refactor constants

---
+++ src/constants/colors.ts
@@ -0,0 +1,32 @@
+const tintColorLight = "#0a7ea4";
+const tintColorDark = "#fff";
+export const validoseWhite = "#FFF";
+export const validoseAqua = "#73D0D7";
+export const validoseAqua3 = "#2E7787";
+export const validoseDarkBlue = "#252F3B";
+export const validoseGrey = "#565F6B";
+export const validoseSuccess = "#65BB8D33";
+export const validoseWarning = "#FFC88399";
+export const validoseMedication = "#E7F0FF";
+export const validoseMedicationError = "#b9d2fa";
+export const validoseMedication1 = "#5D9BFF";
+export const validoseMedication2 = "#345DA0";
+
+export const Colors = {
+  light: {
+    text: "#11181C",
+    background: "#fff",
+    tint: tintColorLight,
+    icon: "#687076",
+    tabIconDefault: "#687076",
+    tabIconSelected: tintColorLight,
+  },
+  dark: {
+    text: "#ECEDEE",
+    background: "#151718",
+    tint: tintColorDark,
+    icon: "#9BA1A6",
+    tabIconDefault: "#9BA1A6",
+    tabIconSelected: tintColorDark,
+  },
+};
