adaddc3b0b709666bfd2a4e33d2088c281194f63
Jasmine Sodhi <jasmine.sodhi@10xbeta.com>
2025-10-09 13:04:39 -0400

    Export logs button style update

--- src/app/(tabs)/logs/index.tsx
+++ src/app/(tabs)/logs/index.tsx
@@ -1,8 +1,9 @@
 import { useFocusEffect } from "@react-navigation/native";
 import { useNavigation } from "expo-router";
 import { useEffect, useRef, useCallback } from "react";
-import { ScrollView, Text, View, Button } from "react-native";
+import { ScrollView, Text, View, Button, Pressable } from "react-native";
 
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { useLogs } from "@/providers/log";
 import { exportLogsToFile } from "@/utils/log";
 
@@ -52,10 +53,30 @@ export default function LogsTab() {
     await exportLogsToFile(filteredLogs.map((log) => log.message));
   };
 
+  const insets = useSafeAreaInsets();
+
   return (
     <View style={{ flex: 1, backgroundColor: "white" }}>
       <View style={{ position: "absolute", top: 50, right: 12, zIndex: 10 }}>
-        <Button title="Export Logs" onPress={handleExport} />
+        {/* <Button title="Export Logs" onPress={handleExport} /> */}
+        <Pressable
+          onPress={handleExport}
+          style={{
+            backgroundColor: "white",
+            paddingVertical: 8,
+            paddingHorizontal: 16,
+            borderRadius: 6,
+            borderWidth: 1,
+            borderColor: "#ccc",
+            shadowColor: "#000",
+            shadowOpacity: 0.1,
+            shadowOffset: { width: 0, height: 2 },
+            shadowRadius: 2,
+            elevation: 2,
+          }}
+        >
+          <Text style={{ color: "#252F3B", fontWeight: "600" }}>Export Logs</Text>
+        </Pressable>
       </View>
       <ScrollView
         ref={scrollRef}
@@ -65,7 +86,10 @@ export default function LogsTab() {
           padding: 12,
           paddingTop: 100,
         }}
-        contentContainerStyle={{ paddingBottom: 32 }}
+        // contentContainerStyle={{ paddingBottom: 32 }}
+        contentContainerStyle={{
+          paddingBottom: insets.bottom + 80, // Enough to stay above tabs
+        }}
         keyboardShouldPersistTaps="handled"
       >
         {filteredLogs.length === 0 && (
